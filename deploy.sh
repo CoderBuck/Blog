81a44b2c8243536fd5f8349a8a241ef90c91f7d0
Buck <coderbuck@163.com>
2020-07-12 16:45:02 +0800

    rebuilding site 2020年07月12日 16:45:02

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t hermit # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t yinyang # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Commit changes.
 msg="rebuilding site $(date)"
