d5f1ba91a40a7fe0d6c1938f71527397ec273905
Buck <coderbuck@163.com>
2020-07-12 17:26:19 +0800

    rebuilding site 2020年07月12日 17:26:19

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t yinyang # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Commit changes.
 msg="rebuilding site $(date)"
@@ -16,12 +16,12 @@ if [ -n "$*" ]; then
 fi
 
 # Add changes to git.
-#git add .
-#
-#git commit -m "$msg"
-#
-## Push source and build repos.
-#git push origin master
+git add .
+
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
 
 # Go To Public folder
 cd public
