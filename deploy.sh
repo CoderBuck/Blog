aaa65be7b9a221a981ecd4d2b384095c20746a84
Buck <coderbuck@163.com>
2020-07-12 18:09:03 +0800

    rebuilding site 2020年07月12日 18:09:03

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo -D # if using a theme, replace with `hugo -t <YOURTHEME>`
